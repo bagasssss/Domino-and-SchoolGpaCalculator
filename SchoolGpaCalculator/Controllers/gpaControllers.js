3d21c69612d47597b70ac3d6e6d70ae7b32c3c8d
bagasssss <lit.bohdan@gmail.com>
2017-01-25 01:46:11 +0200

    follow-ups 1

--- SchoolGpaCalculator/Controllers/gpaControllers.js
+++ SchoolGpaCalculator/Controllers/gpaControllers.js
@@ -1,22 +1,45 @@
 (function(){
 	'use strict';
 
-	angular.module("gpa.controllers", ['gpa.services'])
+	angular.module("gpa.controllers", ['ui.grid', 'gpa.services'])
 		.controller("gpaController", function(gpaService){
+
 			var vm = this;
-			vm.grades = gpaService.grades;
+
+			vm.grades;
+			vm.averageGPA;
 			var idForNewGrade =3;
 			var idForNewStudents = 5;
-			vm.averageGPA;
+			var dataToShow = [];
+
 
 			activate();
 
 			function activate() {
-				calculateAvgGpa();
+				getDataFromFile()
+				
+			};
+
+			function getDataFromFile() {
+				gpaService.getData().success(function(data) {
+					vm.grades = data;
+					calculateAvgGpa();
+					//addStudentsToGradeArray(0);
+
+					var m=[];
+					for(var t=0;t<data.length;t++) {
+						for(var r=0; r<data[t].students.length; r++) {
+							m.push(data[t].students[r]);
+						}
+					}
+					dataToShow = m;
+					
+				})
 			};
 			
 
 			function calculateAvgGpa() {
+				var avgp;
 				var numbersOfStudents = 0;
 				var sumGpa = 0;
 				for(var i=0; i<vm.grades.length; i++){
@@ -25,8 +48,16 @@
 						sumGpa+=vm.grades[i].students[s].gpa;
 					};
 				};
-				vm.averageGPA = Math.round((sumGpa/numbersOfStudents) * 100) / 100;
+				avgp = Math.round((sumGpa/numbersOfStudents) * 100) / 100;
+				if(isNaN(avgp)) {
+					vm.averageGPA = "no students";
+				} else {
+					vm.averageGPA = avgp;
+				}
 			};
+
+
+			/* GRADES*/
 
 			vm.addGrade = function() {
 				var newGrade = {
@@ -37,6 +68,7 @@
 				}
 				vm.grades.push(newGrade);
 				idForNewGrade++;
+
 				console.log("grade added");
 			};
 
@@ -50,8 +82,14 @@
 							break;
 						};
 					};
+					calculateAvgGpa();
 				};
 			};
+
+			//
+			//
+			//
+
 
 			vm.showGrid = function(gradeId) {
 				for(var i=0; i<vm.grades.length; i++) {
@@ -63,7 +101,31 @@
 							vm.grades[i].show = false;
 						};
 					};
+					addStudentsToGradeArray(gradeId);
 			};
+
+			function addStudentsToGradeArray(gradeId) {
+				var st = [];
+				for(var i=0; i<vm.grades.length; i++) {
+					if(vm.grades[i].id === gradeId) {
+						for(var s=0; s<vm.grades[i].students.length; s++) {
+							var student = {
+								name: vm.grades[i].students[s].name,
+								gpa: vm.grades[i].students[s].gpa,
+							};
+
+							st.push(student);
+						};
+						/*st.push(vm.grades[i].students);
+						break;*/
+						
+					};
+				};
+				
+				dataToShow = st;
+				console.log(dataToShow)
+
+			}
 
 			vm.deleteStudent = function(id, name) {
 				for(var i=0; i<vm.grades.length; i++){
@@ -98,6 +160,74 @@
 				};
 				calculateAvgGpa();
 			};
+
+			vm.gridOptions = {};
+			vm.gridOptions.columnDefs = [
+				{field: 'name'},
+				{field: 'gpa'},
+				{name: ' ', celltemplate: '<button class="btn primary" ng-click="ctrl.deleteStudent()">Delete</button>'}
+			];
+/*			vm.gridOptions.data = [
+				{"name": "Vasya Pupkin", "gpa": 4.8, "id_s": 1}
+			];
+*/
+			vm.gridOptions.data = dataToShow;
+
 		})
+
+/*		.controller("studentsCtrl", function(gpaService){
+
+			var vm=this;
+
+			vm.grades;
+			vm.students;
+			vm.averageGPA;
+
+			var idForNewStudent = 5;
+
+			activate();
+
+			function activate() {
+				getDataFromFile();
+			};
+
+			function getDataFromFile() {
+				gpaService.getData().success(function(data) {
+					vm.students = data;
+					calculateAvgGpa();
+					getUniqueGrades();
+				})
+			};
+
+			function getUniqueGrades() {
+				var uGrades = [];
+				for(var i=0; i<vm.students.length; i++) {
+					for(var g=0; g<uGrades.length; g++) {
+						if(vm.students[i].grade !=== uGrades[g]) {
+							uGrades.push(vm.students[i].grade);
+						}
+					}
+				}
+				vm.gardes = uGrades;
+			};
+
+			function calculateAvgGpa() {
+				var numbersOfStudents = 0;
+				var sumGpa = 0;
+				for(var i=0; i<vm.students; i++) {
+					numbersOfStudents++;
+					sumGpa+=vm.students[i].gpa;
+				}
+				vm.averageGPA = Math.round((sumGpa/numbersOfStudents) * 100) / 100;
+			};
+
+
+
+
+
+
+			
+
+		})*/
 
 })();
